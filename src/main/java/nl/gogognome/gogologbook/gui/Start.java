02f76f6dda2e8a96aa4ad8ab85c2571f92df2cd5
Sander Kooijmans <sander@gogognome.nl>
2013-04-29 15:45:45 +0200

    Added Start class to start the application.

--- src/main/java/nl/gogognome/gogologbook/gui/Start.java
+++ src/main/java/nl/gogognome/gogologbook/gui/Start.java
@@ -1,4 +1,5 @@
 package nl.gogognome.gogologbook.gui;
+
 import java.util.Locale;
 
 import javax.swing.JFrame;
@@ -7,22 +8,22 @@ import nl.gogognome.lib.gui.beans.BeanFactory;
 import nl.gogognome.lib.swing.SwingUtils;
 import nl.gogognome.lib.swing.WidgetFactory;
 import nl.gogognome.lib.swing.plaf.DefaultLookAndFeel;
-import nl.gogognome.lib.text.AmountFormat;
 import nl.gogognome.lib.text.TextResource;
 import nl.gogognome.lib.util.Factory;
 
 public class Start {
 
-	private String fileName;
 	private MainFrame mainFrame;
 
 	/**
 	 * Starts the application.
-	 * @param args command line arguments; if one argument is passed, then
-	 *        it is used as file name of an edition that is loaded.
-	 *        Further, if the argument <tt>-lang=X</tt> is used, then
-	 *        the language is set to </tt>X</tt>. </tt>X</tt> should be a valid
-	 *        ISO 639 language code.
+	 *
+	 * @param args
+	 *            command line arguments; if one argument is passed, then it is
+	 *            used as file name of an edition that is loaded. Further, if
+	 *            the argument <tt>-lang=X</tt> is used, then the language is
+	 *            set to </tt>X</tt>. </tt>X</tt> should be a valid ISO 639
+	 *            language code.
 	 */
 	public static void main(String[] args) {
 		Start start = new Start();
@@ -32,12 +33,8 @@ public class Start {
 	private void startApplication(String[] args) {
 		initFactory(Locale.getDefault());
 		parseArguments(args);
-        DefaultLookAndFeel.useDefaultLookAndFeel();
+		DefaultLookAndFeel.useDefaultLookAndFeel();
 		initFrame();
-
-		if (fileName != null) {
-//		    mainFrame.loadFile(fileName);
-		}
 	}
 
 	public void initFactory(Locale locale) {
@@ -46,30 +43,24 @@ public class Start {
 
 		Factory.bindSingleton(TextResource.class, tr);
 		Factory.bindSingleton(WidgetFactory.class, new WidgetFactory(tr));
-		Factory.bindSingleton(AmountFormat.class, new AmountFormat(tr.getLocale()));
 		Factory.bindSingleton(BeanFactory.class, new BeanFactory());
 	}
 
-	/**
-	 * Parses arguments: language must be set before creating main frame
-	 * @param args
-	 */
 	private void parseArguments(String[] args) {
-		for (int i=0; i<args.length; i++)
-		{
+		for (int i = 0; i < args.length; i++) {
 			if (args[i].startsWith("-lang=")) {
 				Locale locale = new Locale(args[i].substring(6));
 				initFactory(locale);
 			} else {
-				fileName = args[i];
+				throw new IllegalArgumentException("Illega argument: " + args[i]);
 			}
 		}
 	}
 
 	private void initFrame() {
 		mainFrame = new MainFrame();
-        mainFrame.setVisible(true);
-        SwingUtils.center(mainFrame);
+		mainFrame.setVisible(true);
+		SwingUtils.center(mainFrame);
 		mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 	}
 
